156def506591baa6de5bdb54965831b60bc929af
prajjwalkumar17 <talk2prajjwal@gmail.com>
2021-05-31 00:58:15 +0530

    added a website model

---
+++ javascript_course/website/js/scripts.js
@@ -0,0 +1,52 @@
+
+// Mobile Menu
+
+
+
+// About us Tab
+
+var aboutUs = {
+  "Mission": "Duis ac leo nisi. Mauris nec ex id lorem commodo rutrum rutrum a est. Cras facilisis sit amet lectus non posuere. Nullam non magna non enim blandit elementum.",
+  "Vision": "Praesent ut lacinia neque, faucibus suscipit quam. Duis sed nunc rutrum, tempor mi at, euismod nibh.",
+  "Values": "<ul><li>Nunc iaculis</li><li>Donec dictum fringilla</li><li>Duis convallis tortor ultrices</li><li>Curabitur in est lectus</li><li>Maecenas condimentum elit</li></ul>"
+};
+
+var unseletectedColor = "#646872";
+var seletectedColor = "#2A2D34";
+
+
+
+
+// Service slider
+
+var ourServices = [
+  {
+    'title': 'Web design',
+    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent finibus tincidunt sem non sodales. Nunc et quam in magna vehicula sollicitudin. Aliquam erat volutpat. Maecenas dolor mi, aliquet ac quam aliquet, condimentum dictum nisi.'
+  },
+
+  {
+    'title': 'Branding',
+    'text': 'Praesent finibus tincidunt sem non sodales. Nunc et quam in magna vehicula sollicitudin. Aliquam erat volutpat. Maecenas dolor mi, aliquet ac quam aliquet, condimentum dictum nisi.'
+  },
+
+  {
+    'title': 'Digital Marketing',
+    'text': 'Nunc et quam in magna vehicula sollicitudin. Aliquam erat volutpat. Maecenas dolor mi, aliquet ac quam aliquet, condimentum dictum nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent finibus.'
+  }
+  
+];
+
+
+
+
+
+// Footer
+
+
+
+  
+   
+
+
+   
